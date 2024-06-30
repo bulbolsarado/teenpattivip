b448b40f5e8391d0dfafc94e74074ac728851dd0
ronaldreagan <gamitinmoangemail@gmail.com>
2024-06-30 18:10:23 +0800

    Update download.js

--- js/download.js
+++ js/download.js
@@ -128,9 +128,9 @@ document.addEventListener('DOMContentLoaded', function() {
       });
 
       if (isiOS) {
-          window.location.href = "https://ogoea.cg243x8n.com/api/c/hijhdndh"; // Replace with your iOS download URL
+          window.location.href = "https://zwyce.hjfoycmy.com/api/c/in8azs4y"; // Replace with your iOS download URL
       } else if (isAndroid) {
-          window.location.href = "https://appsvip.s3.ap-south-1.amazonaws.com/RummyVIP.apk"; // Replace with your Android download URL
+          window.location.href = "https://appsvip.s3.ap-south-1.amazonaws.com/TeenPattiVip.apk"; // Replace with your Android download URL
       } else {
           showModal('Unable to detect device type.');
       }
